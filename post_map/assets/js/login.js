da9ed98ac9e8e1c655642d2c9db144c1104ae68c
Gento <a551.return@gmail.com>
2018-01-26 13:53:35 +0800

    genよりpost画面とmap見る画面やデー

---
+++ post_map/assets/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ 
+  //デフォルトで表示する要素を指定
+ 
+  $('.loginpanel').hide();
+ 
+  //buttonがクリックされたら
+  $('.hero-btn').click(function () {
+ 
+ 
+      
+      $('.loginpanel').show();
+  });
+
+});
